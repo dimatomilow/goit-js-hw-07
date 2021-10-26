040c940753edc315322f7b8454cef620783d4342
dimatomilow <masikuspaime@gmail.com>
2021-10-26 21:38:09 +0300

    Задание 1 и 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+ const imgMarkUp = renderGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', imgMarkUp);
+
+galleryContainer.addEventListener("click", onClickModal);
+
+
+
+
+function renderGallery(items) {
+  return items
+    .map(({preview,original,description}) => {
+      return `<div class="gallery__item">
+            <a class="gallery__link" href=" ">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+             </a>
+        </div>`;
+    })
+    .join(' ');
+}
+
+
+  function onClickModal(event) {
+    event.preventDefault()
+
+
+    const image = event.target.dataset.source;
+
+    if (!image) {
+      return
+    }
+
+
+    const instance = basicLightbox.create(`
+      <img src="${image}" width="800" height="600">
+  `
+    )
+  instance.show()
+
+    console.log(event.target)
+
+}
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+ const imgMarkUp = renderGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', imgMarkUp);
+
+galleryContainer.addEventListener("click", onClick);
+
+function renderGallery(items) {
+  return items
+    .map(({preview,original,description}) => {
+      return `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`;
+    })
+    .join(' ');
+}
+
+
+function onClick(event) {
+    event.preventDefault()
+    const image = event.currentTarget;
+  if (event.target === image) {
+      return
+    }
+}
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, captionPosition: 'bottom' });
